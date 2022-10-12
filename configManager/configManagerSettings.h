628f29157d268fe81d7444d33d65f6a214017c02
MrGreensWorkshop <76992231+MrGreensWorkshop@users.noreply.github.com>
2022-10-12 11:53:42 +0900

    comment and message fix

--- configManager/configManagerSettings.h
+++ configManager/configManagerSettings.h
@@ -15,20 +15,20 @@
 //for Raspberry Pi Pico W, if you set USE_FILE_FS, in Arduino IDE, Click Tools > Flash Size > select FS: 64KB
 #define USE_EEPROM
 
-//adds device name to settings (for plugs) (to disable comment it)
+//adds device name to settings (to disable, comment it)
 #define add_device_name
 
-//adds delete credentials button to settings (to disable comment it)
+//adds delete credentials button to settings (to disable, comment it)
 #define add_delete_credentials_button
 
-//Default Wifi Host Name (to disable set to NULL)
+//Default Wifi Host Name (to disable, set to NULL)
 #define default_wifi_host_name "NewIotDev"
 
-//Default Wifi Credentials (to disable set to NULL)
+//Default Wifi Credentials (to disable, set to NULL)
 #define default_wifi_ssid NULL
 #define default_wifi_pass NULL
 
-//Default Device Name (to disable set to NULL)
+//Default Device Name (to disable, set to NULL)
 #ifdef add_device_name
 #define default_device_name "SmartDevice"
 #endif
@@ -38,11 +38,11 @@
 #define config_ap_ssid "SmartDeviceConfig"
 #define config_ap_pass "adminadmin"
 
-//Firmware update HTTP Auth Credentials (to disable http auth set both to nullptr)
+//Firmware update HTTP Auth Credentials (to disable, http auth set both to nullptr)
 #define config_firm_update_http_user "admin"           
 #define config_firm_update_http_pass "your_long_pass"
 
-//Config mode exit timeout, set 0 to disable it.
+//Config mode exit timeout. (to disable, set to 0)
 #define config_mode_timeout (5UL * 60000UL) //5min
 
 #endif  // CONFIG_MANAGER_SETTINGS_H_
